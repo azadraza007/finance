43e56f87d47b6f7aa6d7487dd0b74f61d2d945ab
shivu150199 <shivamsinghgautam397@gmail.com>
2024-03-17 16:13:04 +0530

    service section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
 import Hero from "./component/Hero";
 import FAQ from "./component/FAQ";
+import Service from "./component/Service";
 function App() {
    return (
     <>
       <Navbar />
       <Hero />
       <FAQ />
+      <Service/>
 
       <Footer />
     </>
